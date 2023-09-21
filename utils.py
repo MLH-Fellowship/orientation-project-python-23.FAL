80ec02d00b31b096874c5f3d556041fa226eecbc
Michael A. Wekesa <Michael A. Wekesa>
2023-09-21 22:31:34 +0300

    add docstring

--- utils.py
+++ utils.py
@@ -2,6 +2,15 @@ from textblob import TextBlob
 
 
 def correct_spellings(sentence: str):
+    """
+    Utility function to correct the spelling of a sentence
+
+    Args:
+        sentence (str): the sentence to be corrected
+
+    Returns:
+        str: the corrected sentence
+    """
     corrected_sentence = TextBlob(sentence).correct()
     return corrected_sentence
 
